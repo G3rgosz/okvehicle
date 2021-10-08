1cad9452aa1c7a0d07cf7ccf8b7f7162ebc5dd74
Gergosz <71034104+Gergosz-2000@users.noreply.github.com>
2021-10-08 14:16:31 +0200

    program atnevezve, fejresz javitva

--- src/App.java
+++ src/App.java
@@ -1,13 +1,15 @@
 /*
  * File: App.java
  * Created Date: 2021-09-24 22:13:27
- * Author: Sallai Andras
- * Github: https://github.com/andteki
+ * Author: Szivak Gergo, Ban Cinti, Hermanyi Gergely
+ * Group: Szoft II N
+ * Original: revehicle, Sallai Andras
+ * Github: https://github.com/Gergosz-2000
  * -----
- * Last Modified: 2021-09-24
- * Modified By: Sallai Andras
+ * Last Modified: 2021-10-08
+ * Modified By: Szivak Gergo, Ban Cinti, Hermanyi Gergely
  * -----
- * Copyright (c) 2021 Sallai Andras
+ * Copyright (c) 2021 Szivak Gergo, Ban Cinti, Hermanyi Gergely
  * 
  * GNU GPL v2
  */
