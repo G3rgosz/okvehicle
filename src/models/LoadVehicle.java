bada7ec57da4bc3fd03bfc8d1f5bb6ae119cf02d
Gergosz <71034104+Gergosz-2000@users.noreply.github.com>
2021-10-12 17:18:45 +0200

    javitva, try kiszervezve, kesz

--- src/models/LoadVehicle.java
+++ src/models/LoadVehicle.java
@@ -6,14 +6,13 @@
  * Original: revehicle, Sallai Andras
  * Github: https://github.com/Gergosz-2000
  * -----
- * Last Modified: 2021-10-11
+ * Last Modified: 2021-10-12
  * Modified By: Szivak Gergo, Ban Cinti, Hermanyi Gergely
  * -----
  * Copyright (c) 2021 Szivak Gergo, Ban Cinti, Hermanyi Gergely
  * 
  * GNU GPL v2
  */
-
 package models;
 
 import java.io.FileNotFoundException;
@@ -22,32 +21,35 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LoadVehicle {
+    
     ArrayList<String> vehicles;
+
     public LoadVehicle() {
         vehicles = new ArrayList<>();
-    }//A LoadVehicle metódus vége
-
+    }
     public ArrayList<Vehicle> load(){
-        //A járművek listáját tárlojuk benne:
         ArrayList<Vehicle> vehicleList = new ArrayList<>();
         try {
-            FileReader fileReader = new FileReader("data.txt");
-            Scanner scanner = new Scanner(fileReader);
-            //Ciklus amivel bejárjuk a fájlt:
-            while(scanner.hasNext()) {
-                String line = scanner.nextLine();
-                String[] lineArray = line.split(":");
-                Vehicle vehicle = new Vehicle();
-                vehicle.ordinal = lineArray[0];
-                vehicle.brand = lineArray[1];
-                vehicle.year = lineArray[2];
-                vehicleList.add(vehicle);
-            }//while vége
-            scanner.close();
-
+            vehicleList = tryLoad();
         } catch (FileNotFoundException e) {
-        System.err.println("Hiba! A fájl nem található");
-        }//A try vége
+            System.err.println("Hiba! A fájl nem található");
+        }
+        return vehicleList;
+    }
+    private ArrayList<Vehicle> tryLoad() throws FileNotFoundException {
+        ArrayList<Vehicle> vehicleList = new ArrayList<>();
+        FileReader fileReader = new FileReader("data.txt");
+        Scanner scanner = new Scanner(fileReader);
+        while(scanner.hasNext()) {
+            String line = scanner.nextLine();
+            String[] lineArray = line.split(":");
+            Vehicle vehicle = new Vehicle();
+            vehicle.ordinal = lineArray[0];
+            vehicle.brand = lineArray[1];
+            vehicle.year = lineArray[2];
+            vehicleList.add(vehicle);
+        }
+        scanner.close();
         return vehicleList;
     }
 }
