0789d45cb2f6f83ba7e6bbbdf7cfc4799e1ce53b
Gergosz <71034104+Gergosz-2000@users.noreply.github.com>
2021-10-12 17:19:24 +0200

    kommentek kiszedve, kesz

--- src/models/Vehicle.java
+++ src/models/Vehicle.java
@@ -1,32 +1,22 @@
 /*
  * File: Vehicle.java
  * Created Date: 2021-09-25 00:50:58
- * Author: Sallai Andras
- * Github: https://github.com/andteki
+ * Author: Szivak Gergo, Ban Cinti, Hermanyi Gergely
+ * Group: Szoft II N
+ * Original: revehicle, Sallai Andras
+ * Github: https://github.com/Gergosz-2000
  * -----
- * Last Modified: 2021-09-25
- * Modified By: Sallai Andras
+ * Last Modified: 2021-10-12
+ * Modified By: Szivak Gergo, Ban Cinti, Hermanyi Gergely
  * -----
- * Copyright (c) 2021 Sallai Andras
+ * Copyright (c) 2021 Szivak Gergo, Ban Cinti, Hermanyi Gergely
  * 
  * GNU GPL v2
  */
-
 package models;
 
-/**
- * A járművek bármilyen kerekes és kerék nélküli eszközök lehetnek.
- * Lehet akár egy Lada, egy szekér. De monjuk az utóbbi típusának
- * a feliírása nem könnyű ebben az oszhtályban. Rendszáma
- * meg pláne nincs a szekereknek, bicikliknek. De mivel
- * személyautókat eladó cégnek készül, jó lesz ez így.
- */
-
 public class Vehicle {
-    // ordinal tároló:
     public String ordinal;
-    // brand tároló:
     public String brand;
-    // year tároló:
     public String year;    
-}// A Vehicle vége
+}
